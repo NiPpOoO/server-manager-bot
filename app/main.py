e530f05983efe8cfc077eeb2b302a444d40212d4
NiPpOoOz <82100496+NiPpOoO@users.noreply.github.com>
2025-05-27 19:49:56 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
+from fastapi import FastAPI
+from app.routes.server_routes import server_bp
 
+app = FastAPI()
+app.include_router(server_bp)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
