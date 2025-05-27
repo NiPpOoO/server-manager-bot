0395c0000ad83e9659a301c7b775d285f4508699
NiPpOoOz <82100496+NiPpOoO@users.noreply.github.com>
2025-05-27 19:48:34 +0300

    Update monitoring.py

--- app/services/monitoring.py
+++ app/services/monitoring.py
@@ -1,1 +1,14 @@
+import psutil
 
+class SystemMonitor:
+    @staticmethod
+    def get_cpu_usage():
+        return psutil.cpu_percent()
+
+    @staticmethod
+    def get_memory_usage():
+        return psutil.virtual_memory().percent
+
+    @staticmethod
+    def get_disk_usage():
+        return psutil.disk_usage('/').percent
