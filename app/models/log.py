18c3be91ee974522f0f884bdc95323b11dcc0e5e
NiPpOoOz <82100496+NiPpOoO@users.noreply.github.com>
2025-05-27 19:47:48 +0300

    Update log.py

--- app/models/log.py
+++ app/models/log.py
@@ -1,1 +1,7 @@
+from app import db
+from datetime import datetime
 
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(500), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
