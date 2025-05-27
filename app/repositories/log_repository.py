6bf1c7ad9b035c9a11c6297fd4afdd31f736f90a
NiPpOoOz <82100496+NiPpOoO@users.noreply.github.com>
2025-05-27 19:48:05 +0300

    Update log_repository.py

--- app/repositories/log_repository.py
+++ app/repositories/log_repository.py
@@ -1,1 +1,9 @@
+from app.models.log import Log
+from app import db
 
+class LogRepository:
+    @staticmethod
+    def save_log(message):
+        log = Log(message=message)
+        db.session.add(log)
+        db.session.commit()
