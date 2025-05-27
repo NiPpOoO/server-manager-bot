2b3ee56e1d96573562646bf3fcb96fb8f3417469
NiPpOoOz <82100496+NiPpOoO@users.noreply.github.com>
2025-05-27 19:52:00 +0300

    Update test_server.py

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,1 +1,14 @@
+import unittest
+from app.services.server_service import ServerService
 
+class ServerTest(unittest.TestCase):
+    def test_restart_server(self):
+        response = ServerService.restart_server()
+        self.assertEqual(response["message"], "Server is restarting")
+
+    def test_shutdown_server(self):
+        response = ServerService.shutdown_server()
+        self.assertEqual(response["message"], "Server is shutting down")
+
+if __name__ == "__main__":
+    unittest.main()
