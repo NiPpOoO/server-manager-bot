20ab456a225b9377dac3c894e6b1e80151be9969
NiPpOoOz <82100496+NiPpOoO@users.noreply.github.com>
2025-05-27 19:51:36 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,6 @@
+import os
 
+class Config:
+    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/server_manager_db")
+    SECRET_KEY = os.getenv("SECRET_KEY", "super_secret_key")
+    DEBUG = os.getenv("DEBUG", True)
