a61f08b52d54d4ecb0f20aa70ac89ac52c1b51c2
NiPpOoOz <82100496+NiPpOoO@users.noreply.github.com>
2025-05-27 19:56:50 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,5 @@
 import os
 
 class Config:
-    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/server_manager_db")
-    SECRET_KEY = os.getenv("SECRET_KEY", "super_secret_key")
-    DEBUG = os.getenv("DEBUG", True)
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://denis_user:secure_password@localhost/server_manager_db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
